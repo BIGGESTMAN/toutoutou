d68e520eb200186f55e0e8129e926c1a0a77e942
Ethan Feinberg <sneakcow@gmail.com>
2015-07-30 17:33:55 -0700

    implemented new invisible full moon version

--- game/dota_addons/barebones/scripts/vscripts/heroes/hero_reisen/invisible_full_moon.lua
+++ game/dota_addons/barebones/scripts/vscripts/heroes/hero_reisen/invisible_full_moon.lua
@@ -1,33 +1,23 @@
-function checkCancelOrder(keys)
-	local order_target = keys.target
+function updateStealth(keys)
 	local caster = keys.caster
 	local ability = keys.ability
-	local unit = keys.unit
+	local ability_level = ability:GetLevel() - 1
+	local target = keys.target
 
-	local order_allowed = true
+	local team = caster:GetTeamNumber()
+	local origin = target:GetAbsOrigin()
+	local iTeam = DOTA_UNIT_TARGET_TEAM_FRIENDLY
+	local iType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC
+	local iFlag = DOTA_UNIT_TARGET_FLAG_NONE
+	local iOrder = FIND_ANY_ORDER
+	local radius = ability:GetLevelSpecialValueFor("radius", ability_level)
+	local allied_units = FindUnitsInRadius(team, origin, nil, radius, iTeam, iType, iFlag, iOrder, false)
 
-	if order_target then
-		local team = caster:GetTeamNumber()
-		local origin = caster:GetAbsOrigin()
-		local iTeam = DOTA_UNIT_TARGET_TEAM_FRIENDLY
-		local iType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC
-		local iFlag = DOTA_UNIT_TARGET_FLAG_NONE
-		local iOrder = FIND_ANY_ORDER
-		local radius = 20100
-		local allied_units = FindUnitsInRadius(team, origin, nil, radius, iTeam, iType, iFlag, iOrder, false)
-
-		for k,allied_unit in pairs(allied_units) do
-			if allied_unit:GetPlayerOwnerID() == caster:GetPlayerID() and allied_unit == order_target then
-				order_allowed = false
-				break
+	for k,unit in pairs(allied_units) do
+		if unit:GetPlayerOwnerID() == caster:GetPlayerID() then
+			if not unit:HasModifier("modifier_stealth_broken") then
+				ability:ApplyDataDrivenModifier(caster, unit, "modifier_true_stealth", {})
 			end
-		end
-
-		if not order_allowed then
-			ability:ApplyDataDrivenModifier(caster, unit, "modifier_disable", {duration = 0.03})
-			Timers:CreateTimer(0.03, function()
-				unit:Stop()
-			end)
 		end
 	end
 end
