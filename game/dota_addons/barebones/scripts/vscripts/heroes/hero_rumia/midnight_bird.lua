aa09f7e89c4566a725b6a4eab958f7c8a8e0bdac
Ethan Feinberg <sneakcow@gmail.com>
2015-08-03 00:29:53 -0700

    implemented preliminary midnight bird

--- game/dota_addons/barebones/scripts/vscripts/heroes/hero_rumia/midnight_bird.lua
+++ game/dota_addons/barebones/scripts/vscripts/heroes/hero_rumia/midnight_bird.lua
@@ -1,86 +1,66 @@
+LinkLuaModifier("modifier_midnight_bird_ordercheck", "heroes/hero_rumia/modifier_midnight_bird_ordercheck.lua", LUA_MODIFIER_MOTION_NONE )
+
 function midnightBirdCast(keys)
-	-- local entity = Entities:CreateByClassname("ent_fow_blocker_node")
-	-- local entity = Entities:CreateByClassname("point_simple_obstruction")
-	-- local entity = SpawnEntityFromTableSynchronous("point_simple_obstruction", {origin = keys.caster:GetAbsOrigin()})
-	-- -- entity:SetAbsOrigin(keys.caster:GetAbsOrigin())
-	-- -- entity:Trigger()
-	-- entity:SetEnabled(true, true)
-	-- -- Timers:CreateTimer(0, function()
-	-- -- 	print(entity:GetAbsOrigin())
-	-- -- 	return 0.5
-	-- -- end)
-
-
-	-- local caster = keys.caster
-	-- local ability = keys.ability
-	-- local ability_level = ability:GetLevel() - 1
-	-- local duration = ability:GetLevelSpecialValueFor("duration", ability_level)
-	-- local radius = ability:GetLevelSpecialValueFor("radius", ability_level)
-	
-	-- local target_point = caster:GetAbsOrigin()
-	-- local tree_count = 8
-	-- for i=1,tree_count do
-	-- 	local vector = RotatePosition(Vector(0,0,0), QAngle(0,i * 360 / tree_count,0), caster:GetForwardVector())
-	-- 	local target = target_point + vector * radius
-	-- 	CreateTempTree(target, duration)
-	-- end
-
-	-- local trees = GridNav:GetAllTreesAroundPoint(target_point, radius, true)
-	-- for k,v in pairs(trees) do
-	-- 	for k2,v2 in pairs(v) do
-	-- 		print(k2,v2)
-	-- 	end
-	-- end
-
 	local caster = keys.caster
 	local ability = keys.ability
 	local ability_level = ability:GetLevel() - 1
 
 	GameRules:SetCustomGameTeamMaxPlayers(6, 10)
-	local dummy_unit = CreateUnitByName("npc_dummy_unit", caster:GetAbsOrigin(), false, caster, caster, caster:GetTeam())
-	ability:ApplyDataDrivenModifier(caster, dummy_unit, keys.dummy_modifier, {})
-	local vision_dummy = CreateUnitByName("npc_dummy_unit", caster:GetAbsOrigin(), false, caster, caster, 6)
-	ability:ApplyDataDrivenModifier(caster, vision_dummy, "modifier_midnight_bird_vision_dummy", {})
-	Timers:CreateTimer(ability:GetLevelSpecialValueFor("duration", ability_level), function()
-		vision_dummy:RemoveSelf()
-	end)
+	local playerID = caster:GetPlayerID()
+	PlayerResource:SetCustomTeamAssignment(playerID, 6)
+
+	ability:ApplyDataDrivenModifier(caster, caster, "modifier_midnight_bird", {})
+	caster:AddNewModifier(caster, ability, "modifier_midnight_bird_ordercheck", {})
+	caster:AddNoDraw()
+	caster.midnight_units_eaten = {}
+
+	caster.midnight_vision_dummy = CreateUnitByName("npc_dummy_unit", caster:GetAbsOrigin(), false, caster, caster, 6)
+	ability:ApplyDataDrivenModifier(caster, caster.midnight_vision_dummy, "modifier_midnight_bird_vision_dummy", {})
 	-- AddFOWViewer(6, dummy_unit:GetAbsOrigin(), ability:GetLevelSpecialValueFor("radius", ability_level), ability:GetLevelSpecialValueFor("duration", ability_level), true)
 
-	Timers:CreateTimer(ability:GetLevelSpecialValueFor("duration", ability_level), function()
-		dummy_unit:RemoveSelf()
-	end)
+	caster.midnight_particle = ParticleManager:CreateParticle("particles/rumia/midnight_bird_cloud.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
+	ParticleManager:SetParticleControlEnt(caster.midnight_particle, 2, caster, PATTACH_ABSORIGIN, "attach_origin", caster:GetAbsOrigin(), true)
+
+	-- Enable early cancel ability
+	local main_ability_name	= ability:GetAbilityName()
+	local sub_ability_name	= "midnight_bird_end"
+	caster:SwapAbilities(main_ability_name, sub_ability_name, false, true)
 end
 
-function midnightBirdAOE(keys)
-	-- print("!")
-	-- local caster = keys.caster
-	-- local ability = keys.ability
-	-- local ability_level = ability:GetLevel() - 1
-	-- local target = keys.target
+function midnightBirdEnd(keys)
+	local caster = keys.target
 
-	-- local team = caster:GetTeamNumber()
-	-- local origin = target:GetAbsOrigin()
-	-- local iTeam = DOTA_UNIT_TARGET_TEAM_BOTH
-	-- local iType = DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_HERO
-	-- local iFlag = DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES
-	-- local iOrder = FIND_ANY_ORDER
-	-- local radius = ability:GetLevelSpecialValueFor("radius", ability_level)
+	local playerID = caster:GetPlayerID()
+	PlayerResource:SetCustomTeamAssignment(playerID, caster:GetTeamNumber())
 
-	-- local targets = FindUnitsInRadius(team, origin, nil, radius, iTeam, iType, iFlag, iOrder, false)
+	caster:RemoveNoDraw()
+	caster:RemoveModifierByName("modifier_midnight_bird_ordercheck")
 
-	-- for k,unit in pairs(targets) do
-	-- 	print("?")
-	-- 	ability:ApplyDataDrivenModifier(caster, unit, "modifier_midnight_bird_blinded", {})
-	-- end
+	caster.midnight_vision_dummy:RemoveSelf()
+	ParticleManager:DestroyParticle(caster.midnight_particle, false)
+
+	for unit,v in pairs(caster.midnight_units_eaten) do
+		unit:RemoveModifierByName("modifier_midnight_bird_eaten")
+		FindClearSpaceForUnit(unit, caster:GetAbsOrigin(), false)
+		if unit:GetTeamNumber() ~= caster:GetTeamNumber() then
+			ApplyDamage({ victim = unit, attacker = caster, damage = 250, damage_type = DAMAGE_TYPE_MAGICAL})
+		end
+		unit:RemoveNoDraw()
+
+		local unitPlayerID = unit:GetPlayerID()
+		PlayerResource:SetCustomTeamAssignment(unitPlayerID, unit:GetTeamNumber())
+	end
+
+	-- Disable retarget ability
+	local main_ability_name	= keys.ability:GetAbilityName()
+	local sub_ability_name	= "midnight_bird_end"
+	caster:SwapAbilities(main_ability_name, sub_ability_name, true, false)
 end
 
-function blinded(keys)
-	local playerID = keys.target:GetPlayerID()
-	PlayerResource:SetCustomTeamAssignment(playerID, 6)
-	print(PlayerResource:GetTeam(playerID))
+function updateVisionAOE(keys)
+	keys.target.midnight_vision_dummy:SetAbsOrigin(keys.target:GetAbsOrigin())
 end
 
-function unBlinded(keys)
-	local playerID = keys.target:GetPlayerID()
-	PlayerResource:SetCustomTeamAssignment(playerID, keys.target:GetTeamNumber())
+function cancel(keys)
+	keys.caster:RemoveModifierByName("modifier_midnight_bird")
 end
