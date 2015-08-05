8facc7c164b2274a874c5ea8807441912da98918
Ethan Feinberg <sneakcow@gmail.com>
2015-08-05 00:50:27 -0700

    added indicator for number of stored dolls in a tripwire, fixed wire particles not despawning, prevented re-attaching of tripwires which would instantly break the wire due to distance

--- game/dota_addons/barebones/scripts/vscripts/heroes/hero_alice/trip_wire.lua
+++ game/dota_addons/barebones/scripts/vscripts/heroes/hero_alice/trip_wire.lua
@@ -16,7 +16,7 @@ function createWire(keys)
 		-- Wire is table with three values--each of its two attachee units and the unit for itself
 		local wire = {keys.caster, keys.target, wire_unit}
 		if not caster.wires then caster.wires = {} end
-		caster.wires[wire] = true
+		caster.wires[wire_unit] = wire
 
 		wire_unit.ally_particle = ParticleManager:CreateParticleForTeam("particles/alice/trip_wire.vpcf", PATTACH_POINT_FOLLOW, wire[1], caster:GetTeamNumber())
 
@@ -42,81 +42,88 @@ function updateWire(keys)
 	local caster = keys.caster
 	local ability = keys.ability
 	local ability_level = ability:GetLevel() - 1
-	for wire,v in pairs(caster.wires) do
-		local target1 = wire[1]
-		local target2 = wire[2]
-		local range = nil
-		if not target1:IsNull() and not target2:IsNull() then -- make sure both wire attachees still exist
-			range = (target2:GetAbsOrigin() - target1:GetAbsOrigin()):Length2D()
+	local wire = caster.wires[keys.target]
+	local target1 = wire[1]
+	local target2 = wire[2]
+	local range = nil
+	if not target1:IsNull() and not target2:IsNull() then -- make sure both wire attachees still exist
+		range = (target2:GetAbsOrigin() - target1:GetAbsOrigin()):Length2D()
+	end
+	if range and range <= ability:GetLevelSpecialValueFor("max_length", ability_level) and target1:IsAlive() and target2:IsAlive() then
+		-- Update wire unit position
+		wire[3]:SetAbsOrigin((target1:GetAbsOrigin() + target2:GetAbsOrigin()) / 2 + Vector(0,0,50))
+
+		-- Particle shit
+		-- Check visibility of wire to enemy team
+		local dummy_enemy = CreateUnitByName("npc_dummy_unit", Vector(0,0,0), false, caster, caster, caster:GetOpposingTeamNumber())
+		local visibleToEnemies = dummy_enemy:CanEntityBeSeenByMyTeam(wire[3])
+		dummy_enemy:RemoveSelf()
+
+		-- Update ally particle
+		ParticleManager:SetParticleControl(wire[3].ally_particle,0,Vector(target1:GetAbsOrigin().x,target1:GetAbsOrigin().y,target1:GetAbsOrigin().z + target1:GetBoundingMaxs().z ))	
+		ParticleManager:SetParticleControl(wire[3].ally_particle,1,Vector(target2:GetAbsOrigin().x,target2:GetAbsOrigin().y,target2:GetAbsOrigin().z + target2:GetBoundingMaxs().z ))
+
+		-- Create/destroy and update enemy particle
+		local particle = nil
+		if not visibleToEnemies and wire[3].enemy_particle then
+			ParticleManager:DestroyParticle(wire[3].enemy_particle, false)
+			wire[3].enemy_particle = nil
+		elseif visibleToEnemies and not wire[3].enemy_particle then
+			wire[3].enemy_particle = ParticleManager:CreateParticleForTeam("particles/alice/trip_wire.vpcf", PATTACH_POINT_FOLLOW, wire[1], caster:GetOpposingTeamNumber())
+			ParticleManager:SetParticleControl(wire[3].enemy_particle,0,Vector(target1:GetAbsOrigin().x,target1:GetAbsOrigin().y,target1:GetAbsOrigin().z + target1:GetBoundingMaxs().z ))	
+			ParticleManager:SetParticleControl(wire[3].enemy_particle,1,Vector(target2:GetAbsOrigin().x,target2:GetAbsOrigin().y,target2:GetAbsOrigin().z + target2:GetBoundingMaxs().z ))
 		end
-		if range and range <= ability:GetLevelSpecialValueFor("max_length", ability_level) and target1:IsAlive() and target2:IsAlive() and not wire[3]:IsNull() and wire[3]:IsAlive() then
-			-- Update wire unit position
-			wire[3]:SetAbsOrigin((target1:GetAbsOrigin() + target2:GetAbsOrigin()) / 2 + Vector(0,0,50))
 
-			-- Particle shit
-			-- Check visibility of wire to enemy team
-			local dummy_enemy = CreateUnitByName("npc_dummy_unit", Vector(0,0,0), false, caster, caster, caster:GetOpposingTeamNumber())
-			local visibleToEnemies = dummy_enemy:CanEntityBeSeenByMyTeam(wire[3])
-			dummy_enemy:RemoveSelf()
+		-- If dolls are stored, update indicator particle
+		if not wire[3].indicator_particle then
+			wire[3].indicator_particle = ParticleManager:CreateParticleForTeam("particles/alice/trip_wire_dolls_stored_indicator.vpcf", PATTACH_OVERHEAD_FOLLOW, wire[3], caster:GetTeamNumber())
+		end
+		ParticleManager:SetParticleControl(wire[3].indicator_particle, 1, Vector(0, #wire[3].stored_dolls, 0))
 
-			-- Update ally particle
-			ParticleManager:SetParticleControl(wire[3].ally_particle,0,Vector(target1:GetAbsOrigin().x,target1:GetAbsOrigin().y,target1:GetAbsOrigin().z + target1:GetBoundingMaxs().z ))	
-			ParticleManager:SetParticleControl(wire[3].ally_particle,1,Vector(target2:GetAbsOrigin().x,target2:GetAbsOrigin().y,target2:GetAbsOrigin().z + target2:GetBoundingMaxs().z ))
 
-			-- Create/destroy and update enemy particle
-			local particle = nil
-			if not visibleToEnemies and wire[3].enemy_particle then
-				ParticleManager:DestroyParticle(wire[3].enemy_particle, false)
-				wire[3].enemy_particle = nil
-			elseif visibleToEnemies and not wire[3].enemy_particle then
-				wire[3].enemy_particle = ParticleManager:CreateParticleForTeam("particles/alice/trip_wire.vpcf", PATTACH_POINT_FOLLOW, wire[1], caster:GetOpposingTeamNumber())
-				ParticleManager:SetParticleControl(wire[3].enemy_particle,0,Vector(target1:GetAbsOrigin().x,target1:GetAbsOrigin().y,target1:GetAbsOrigin().z + target1:GetBoundingMaxs().z ))	
-				ParticleManager:SetParticleControl(wire[3].enemy_particle,1,Vector(target2:GetAbsOrigin().x,target2:GetAbsOrigin().y,target2:GetAbsOrigin().z + target2:GetBoundingMaxs().z ))
+		-- Check for enemy units in wire's path
+		local thinker_modifier = "modifier_dummy"
+		local radius = ability:GetLevelSpecialValueFor("radius", ability_level)
+		local direction = (target2:GetAbsOrigin() - target1:GetAbsOrigin()):Normalized()
+
+		local hit_units = unitsInLine(caster, ability, thinker_modifier, target1:GetAbsOrigin(), range, radius, direction, false)
+
+		if #hit_units > 0 then
+			wire_triggered = false -- Make sure attaching to an enemy doesn't instantly break wire
+			for k,unit in pairs(hit_units) do
+				if unit ~= target1 and unit ~= target2 then
+					wire_triggered = true
+				end
 			end
 
-
-			-- Check for enemy units in wire's path
-			local thinker_modifier = keys.thinker_modifier
-			local radius = ability:GetLevelSpecialValueFor("radius", ability_level)
-			local direction = (target2:GetAbsOrigin() - target1:GetAbsOrigin()):Normalized()
-
-			local hit_units = unitsInLine(caster, ability, thinker_modifier, target1:GetAbsOrigin(), range, radius, direction, false)
-
-			if #hit_units > 0 then
-				wire_triggered = false -- Make sure attaching to an enemy doesn't instantly break wire
+			if wire_triggered then
 				for k,unit in pairs(hit_units) do
-					if unit ~= target1 and unit ~= target2 then
-						wire_triggered = true
-					end
+					ApplyDamage({victim = unit, attacker = caster, damage = ability:GetLevelSpecialValueFor("damage", ability_level), damage_type = DAMAGE_TYPE_MAGICAL})
+					ability:ApplyDataDrivenModifier(caster, unit, "modifier_trip_wire_root", {})
 				end
 
-				if wire_triggered then
-					for k,unit in pairs(hit_units) do
-						ApplyDamage({victim = unit, attacker = caster, damage = ability:GetLevelSpecialValueFor("damage", ability_level), damage_type = DAMAGE_TYPE_MAGICAL})
-						ability:ApplyDataDrivenModifier(caster, unit, keys.root_modifier, {})
-					end
-
-					local wire_location = wire[3]:GetAbsOrigin()
-					local little_legion_ability = caster:FindAbilityByName("little_legion")
-					for i=1,#wire[3].stored_dolls do
-						Timers:CreateTimer(i * ability:GetLevelSpecialValueFor("delay_between_doll_spawns", ability_level), function()
-							spawnDoll(little_legion_ability, hit_units[RandomInt(1, #hit_units)], caster, wire_location)
-						end)
-					end
-
-					destroyWire(wire, caster)
+				local wire_location = wire[3]:GetAbsOrigin()
+				local little_legion_ability = caster:FindAbilityByName("little_legion")
+				for i=1,#wire[3].stored_dolls do
+					Timers:CreateTimer(i * ability:GetLevelSpecialValueFor("delay_between_doll_spawns", ability_level), function()
+						spawnDoll(little_legion_ability, hit_units[RandomInt(1, #hit_units)], caster, wire_location)
+					end)
 				end
+
+				destroyWire(wire, caster)
 			end
-		else
-			destroyWire(wire, caster)
 		end
+	else
+		destroyWire(wire, caster)
 	end
 end
 
 function attach(keys)
 	local caster = keys.caster
 	local target = keys.target
-	if caster.last_wire[2] ~= target and caster ~= target then
+	local wire_distance = (caster.last_wire[2]:GetAbsOrigin() - target:GetAbsOrigin()):Length2D()
+	local wire_ability = caster:FindAbilityByName("trip_wire")
+	if caster.last_wire[2] ~= target and caster ~= target and wire_distance <= wire_ability:GetLevelSpecialValueFor("max_length", wire_ability:GetLevel() - 1) then
 		caster.last_wire[1] = target
 		local main_ability_name	= keys.main_ability_name
 		local sub_ability_name	= keys.ability:GetAbilityName()
@@ -124,11 +131,17 @@ function attach(keys)
 	end
 end
 
+function onWireDeath(keys)
+	destroyWire(keys.caster.wires[keys.target], keys.caster)
+end
+
 function destroyWire(wire, caster)
+	-- Clean out particles
 	ParticleManager:DestroyParticle(wire[3].ally_particle, false)
 	if wire[3].enemy_particle then ParticleManager:DestroyParticle(wire[3].enemy_particle, false) end
+	if wire[3].indicator_particle then ParticleManager:DestroyParticle(wire[3].indicator_particle, false) end
 
-	caster.wires[wire] = nil
+	caster.wires[wire[3]] = nil
 	if caster:FindAbilityByName("trip_wire"):IsHidden() and wire == caster.last_wire then
 		caster:SwapAbilities("trip_wire", "trip_wire_attach", true, false)
 	end
