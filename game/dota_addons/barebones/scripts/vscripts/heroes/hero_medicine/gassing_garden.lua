945bcd8e1caab630cd51009a1970b7752d308f77
Ethan Feinberg <sneakcow@gmail.com>
2015-08-11 18:51:13 -0700

    changed gassing garden particle

--- game/dota_addons/barebones/scripts/vscripts/heroes/hero_medicine/gassing_garden.lua
+++ game/dota_addons/barebones/scripts/vscripts/heroes/hero_medicine/gassing_garden.lua
@@ -12,3 +12,15 @@ function gassingGardenHit(keys)
 		ApplyDamage({ victim = target, attacker = caster, damage = damage, damage_type = DAMAGE_TYPE_MAGICAL})
 	end
 end
+
+function createParticle(keys)
+	local caster = keys.caster
+	local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1
+	local target = keys.target_points[1]
+	local thinker = keys.target
+
+	local particle = ParticleManager:CreateParticle("particles/medicine/gassing_garden.vpcf", PATTACH_ABSORIGIN_FOLLOW, thinker)
+	ParticleManager:SetParticleControl(particle, 1, Vector(ability:GetLevelSpecialValueFor("radius", ability_level),1,1))
+	ParticleManager:SetParticleControl(particle, 2, Vector(ability:GetLevelSpecialValueFor("duration", ability_level),0,0))
+end
