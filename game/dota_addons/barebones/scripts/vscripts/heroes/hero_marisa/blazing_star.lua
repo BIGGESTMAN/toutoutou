c0da2684d0fa770956321b6944be4b163aafa09d
Ethan Feinberg <sneakcow@gmail.com>
2015-08-18 14:50:37 -0700

    implemented foreseeing nyuudou cloud

--- game/dota_addons/barebones/scripts/vscripts/heroes/hero_marisa/blazing_star.lua
+++ game/dota_addons/barebones/scripts/vscripts/heroes/hero_marisa/blazing_star.lua
@@ -14,7 +14,7 @@ function blazingStarStart( keys )
 	
 	local master_spark_ability = caster:FindAbilityByName("master_spark")
 	if master_spark_ability:GetLevel() > 0 then
-		startSpark(caster, master_spark_ability, "modifier_master_spark", "modifier_master_spark_slow", caster:GetForwardVector() * -1)
+		startSpark(caster, master_spark_ability, "modifier_master_spark", "modifier_master_spark_slow", caster:GetForwardVector() * -1, master_spark_ability, "particles/marisa/master_spark.vpcf")
 	end
 
 	-- Enable reverse-direction ability
