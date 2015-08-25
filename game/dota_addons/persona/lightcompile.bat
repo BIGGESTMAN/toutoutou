76701aff3821cc9a166d45957ed914a7f04c8978
Ethan Feinberg <sneakcow@gmail.com>
2015-08-25 12:27:07 -0700

    split items file into separate files

--- game/dota_addons/persona/lightcompile.bat
+++ game/dota_addons/persona/lightcompile.bat
@@ -1,3 +1,4 @@
 C:\Python34\python.exe "C:\SteamLibrary\SteamApps\common\dota 2 beta\game\dota_addons\persona\make_npc_abilities_custom.py"
+C:\Python34\python.exe "C:\SteamLibrary\SteamApps\common\dota 2 beta\game\dota_addons\persona\make_npc_items_custom.py"
 C:\Python34\python.exe "C:\SteamLibrary\SteamApps\common\dota 2 beta\game\dota_addons\persona\generatetooltips.py"
 C:\Python34\python.exe "C:\SteamLibrary\SteamApps\common\dota 2 beta\game\dota_addons\persona\make_addon_english.py"
