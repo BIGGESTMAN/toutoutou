f58edef445f0401d92cbc0cd22a704a916fe3d49
Ethan Feinberg <sneakcow@gmail.com>
2015-08-31 09:46:05 -0700

    added slash of departing same-target cooldown

--- game/dota_addons/barebones/scripts/vscripts/heroes/hero_youmu/slash_of_departing.lua
+++ game/dota_addons/barebones/scripts/vscripts/heroes/hero_youmu/slash_of_departing.lua
@@ -26,35 +26,40 @@ function spellCast(keys)
 	local caster = keys.caster
 	local ability = keys.ability
 	local ally = keys.target
-
-	local healing = 0
-	for time,damage in pairs(ally.slash_of_departing_damage_instances) do
-		if GameRules:GetGameTime() - time <= ability:GetSpecialValueFor("heal_period") then
-			healing = healing + damage
+	if not ally:HasModifier("modifier_slash_of_departing_cast_recently") then
+		local healing = 0
+		for time,damage in pairs(ally.slash_of_departing_damage_instances) do
+			if GameRules:GetGameTime() - time <= ability:GetSpecialValueFor("heal_period") then
+				healing = healing + damage
+			end
 		end
-	end
-	local max_healing = ability:GetSpecialValueFor("max_heal")
-	if healing > max_healing then healing = max_healing end
+		local max_healing = ability:GetSpecialValueFor("max_heal")
+		if healing > max_healing then healing = max_healing end
 
-	-- Karmic punishment charge
-	-- Store healing as damage
-	caster.slash_of_departing_stored_damage = healing
-	ability:ApplyDataDrivenModifier(caster, caster, "modifier_slash_of_departing_charge_stored", {})
-	-- Store debuffs
-	caster.slash_of_departing_stored_debuffs = {}
-	local modifiers = ally:FindAllModifiers()
-	for _,modifier in pairs(modifiers) do
-		local modifier_caster = modifier:GetCaster()
-		local modifier_duration = modifier:GetDieTime() - modifier:GetCreationTime()
-		if modifier_caster:GetTeamNumber() ~= caster:GetTeamNumber() and modifier_duration > 0 then
-			local modifier_ability = modifier:GetAbility()
-			local modifier_name = modifier:GetName()
-			table.insert(caster.slash_of_departing_stored_debuffs, {ability = modifier_ability, name = modifier_name, duration = modifier_duration})
+		-- Karmic punishment charge
+		-- Store healing as damage
+		caster.slash_of_departing_stored_damage = healing
+		ability:ApplyDataDrivenModifier(caster, caster, "modifier_slash_of_departing_charge_stored", {})
+		-- Store debuffs
+		caster.slash_of_departing_stored_debuffs = {}
+		local modifiers = ally:FindAllModifiers()
+		for _,modifier in pairs(modifiers) do
+			local modifier_caster = modifier:GetCaster()
+			local modifier_duration = modifier:GetDieTime() - modifier:GetCreationTime()
+			if modifier_caster:GetTeamNumber() ~= caster:GetTeamNumber() and modifier_duration > 0 then
+				local modifier_ability = modifier:GetAbility()
+				local modifier_name = modifier:GetName()
+				table.insert(caster.slash_of_departing_stored_debuffs, {ability = modifier_ability, name = modifier_name, duration = modifier_duration})
+			end
 		end
-	end
 
-	ally:Heal(healing, caster)
-	ally:Purge(false, true, false, true, true)
+		ally:Heal(healing, caster)
+		ally:Purge(false, true, false, true, true)
+		ability:ApplyDataDrivenModifier(caster, ally, "modifier_slash_of_departing_cast_recently", {})
+	else
+		ability:RefundManaCost()
+		ability:EndCooldown()
+	end
 end
 
 function abilityLearned(keys)
