d9c8ff8b9d13048ed816692bccd87557ca9cc686
Ethan Feinberg <sneakcow@gmail.com>
2015-12-17 02:27:11 -0800

    Lunar dial now freezes modifiers (probably breaking all sorts of shit)

--- game/dota_addons/barebones/scripts/vscripts/heroes/hero_sakuya/lunar_dial.lua
+++ game/dota_addons/barebones/scripts/vscripts/heroes/hero_sakuya/lunar_dial.lua
@@ -16,6 +16,10 @@ function spellCast(keys)
 	for k,unit in pairs(targets) do
 		if unit ~= caster then
 			ability:ApplyDataDrivenModifier(caster, unit, "modifier_lunar_dial_frozen", {})
+			for _,modifier in pairs(unit:FindAllModifiers()) do
+				modifier.pre_frozen_duration = modifier:GetRemainingTime()
+				if modifier.pre_frozen_duration <= 0 then modifier.pre_frozen_duration = nil end
+			end
 		end
 	end
 	ability:ApplyDataDrivenModifier(caster, caster, "modifier_lunar_dial_tracker", {})
@@ -35,13 +39,21 @@ function updateTick(keys)
 	local radius = 20100
 	local iTeam = DOTA_UNIT_TARGET_TEAM_BOTH
 	local iType = DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_MECHANICAL + DOTA_UNIT_TARGET_BUILDING
-	local iFlag = DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES
+	local iFlag = DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES + DOTA_UNIT_TARGET_FLAG_INVULNERABLE
 	local iOrder = FIND_ANY_ORDER
 	local targets = FindUnitsInRadius(team, origin, nil, radius, iTeam, iType, iFlag, iOrder, false)
 	for k,unit in pairs(targets) do
-		if unit ~= caster and not unit:HasModifier("modifier_lunar_dial_frozen") then
-			ability:ApplyDataDrivenModifier(caster, unit, "modifier_lunar_dial_frozen", {})
-			unit:FindModifierByName("modifier_lunar_dial_frozen"):SetDuration(duration_remaining, true)
+		if unit ~= caster then
+			if not unit:HasModifier("modifier_lunar_dial_frozen") then
+				ability:ApplyDataDrivenModifier(caster, unit, "modifier_lunar_dial_frozen", {})
+				local modifier = unit:FindModifierByName("modifier_lunar_dial_frozen")
+				if modifier then modifier:SetDuration(duration_remaining, true) end
+			end
+			for _,modifier in pairs(unit:FindAllModifiers()) do
+				if modifier:GetName() ~= "modifier_lunar_dial_frozen" and modifier.pre_frozen_duration then
+					modifier:SetDuration(modifier.pre_frozen_duration, true)
+				end
+			end
 		end
 	end
 
@@ -61,5 +73,9 @@ function frozenDurationEnded(keys)
 		local damage = ability:GetSpecialValueFor("damage")
 		local damage_type = ability:GetAbilityDamageType()
 		ApplyDamage({victim = target, attacker = caster, damage = damage, damage_type = damage_type})
+	end
+
+	for _,modifier in pairs(target:FindAllModifiers()) do
+		modifier.pre_frozen_duration = nil
 	end
 end
